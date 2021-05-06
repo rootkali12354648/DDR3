b234fe782226c73ee60f2bf8fa6dbf2d8ec31d26
rootkali12354648 <46966920+rootkali12354648@users.noreply.github.com>
2021-05-07 01:32:22 +0700

    Add files via upload

---
+++ rTest.sh
@@ -0,0 +1,30 @@
+clear
+echo -e "                                                                                    "
+echo -e "                               \e[1;36m Aircrack-ng \e[0;32m1.6                          \e[32m           "
+echo -e "                                                                                    "
+echo -e "       [\e[31m00:00:10\e[32m] \e[31m39293\e[32m/\e[31m39327\e[32m keys tested \e[35m(4181.97 k/s)                        \e[32m     "
+echo -e "                                                                                    "
+echo -e "       Time left: \e[31m0\e[32m seconds                                     \e[1;32m 99.91\e[0;32m%        \e[32m     "
+echo -e "                                                                                    "
+echo -e "                           \e[1;33m KEY FOUND\e[0;31m! \e[32m[ \e[1;37m0979808320 \e[0;32m]                                 "
+echo -e "                                                                                    "
+echo -e "                                                                                    "
+echo -e "       Master \e[33mKey     \e[32m: \e[31mCD D7 9A 5A CF B0 70 C7 E9 D1 02 3B 87 02 85 D6  \e[32m           "
+echo -e "                        \e[31m39 E4 30 B3 2F 31 AA 37 AC 82 5A 55 B5 55 24 EE  \e[32m           "
+echo -e "                                                                               \e[32m     "
+echo -e "       Transient \e[33mKey  \e[32m: \e[31m9F 2C 65 E4 A6 08 F2 5A 67 97 D9 6F 76 5B 8C D3  \e[32m           "
+echo -e "                        \e[31mDF 13 2F BC DA 6A 6E D9 62 CD D7 38 37 D0 47 4F  \e[32m           "
+echo -e "                        \e[31mE5 AA F6 DC F2 6D 9F 7C 2E DA D7 FA 1E B2 85 00  \e[32m           "
+echo -e "                        \e[1;31m00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  \e[0;32m           "
+echo -e "                                                                               \e[32m     "
+echo -e "       \e[36mEAPOL HMAC    \e[32m : \e[31m28 A8 C8 95 B7 17 E5 72 27 B6 A7 EE E3 E5 34 45  \e[32m           "
+echo -e "                                                                                    "
+echo -e "                                                                               \e[32m     "
+sleep 1
+echo -e "  \e[1;92mInFiled \e[0;32m. . .  \e[33mOK \e[32m[\e[31m1\e[32m]                                                    \e[32m     "
+sleep 1
+echo -e "  \e[1;92mCracked \e[0;32m. . .  \e[33mOK \e[32m[\e[31mwifi.cap\e[32m]                                                 \e[32m     "
+sleep 1
+echo -e "  \e[1;92mSuckerS \e[0;32m. . .  \e[33mOK \e[32m[\e[31mpassword.lst\e[32m]                                             \e[32m     "
+sleep 1
+echo -e " Press any key to continue . . .                                                    "
